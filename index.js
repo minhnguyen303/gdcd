9e56c629aed149466af2bcf260baac8a1f808999
Minh Nguyễn <minhnh@arrowhitech.net>
2022-04-27 21:02:25 +0700

    test

---
+++ index.js
@@ -0,0 +1,192 @@
+jQuery(document).ready(function($) {
+    let questions = ` Nhà nước pháp quyền là nhà nước quản lý mọi mặt của đời sống xã hội bằng
+|?|A. Thói quen.|			|B. Tín ngưỡng. 	|	|C. Pháp luật.	|		|D. Tập quán.
+|?|C|-|
+ Nền dân chủ XHCN mang bản chất của giai cấp
+|?|A. quần chúng nhân dân.				|B. những người quản lý,
+|C. giai cấp công nhân.				|D. giai cấp nông dân.
+|?|C|-|
+ Một trong những phương hưởng cơ bản để thực hiện tốt chính sách dân số ở nước ta hiện nay đó là triển khai tốt nội dung nào dưới đây ?
+|?|A. Từ bỏ mọi hủ tục vùng miền			|B. Tăng cường công tác lãnh đạo và quản lý
+|C. Cần hủy bỏ mọi giao dịch dân sự			|D. Tiềm lực tài chính vững mạnh
+|?|B|-|
+ Một trong những phương hướng cơ bản để thực hiện tốt chính sách dân số ở nước ta hiện này đó là triển khai tốt nội dung nào dưới đây?
+|?|A. Chiến lược phân bố dân cư			|B. Việc chia đều của cải xã hội.
+|C. Thực hiện quan hệ giao tiếp. 			|D. Xây dựng bộ máy từ Trung ương đến cơ sở
+|?|A|-|
+ Một trong những phương hướng cơ bản của chính sách tài nguyên và bảo vệ môi trường ở nước ta được thể hiện ở nội dung nào sau đây?
+|?|A. Tăng cường công tác quản lý nhà nước. 		|B. Lắp đặt hệ thống phần mềm quản lí.
+|C. phát triển kinh tế du lịch cộng đồng.		|D. kiểm tra việc sử dụng các loại phí.
+|?|A|-|
+ Đối với nước ta hiện nay, một trong những nhiệm vụ của chính sách giáo dục và đào tạo là góp phần
+|?|A. bảo mật chương trình học.				|B. Định hướng đối mới giáo dục,
+|C. miễn học phí toàn phần.				|D. Nâng cao dân trí
+|?|D|-|
+ Đối với nước ta hiện nay, một trong những nhiệm vụ của chính sách giáo dục và đào tạo là góp phần
+|?|A. Đào tạo nhân lực.					|B. học bất cứ nơi nào.
+|C. hưởng tất cả chế độ ưu đãi.			|D. duy trì mọi phúc lợi xã hội.
+|?|A|-|
+ Đối với nước ta hiện nay, một trong những nhiệm vụ của chính sách giáo dục và đào tạo là góp phần
+|?|A. Bồi dưỡng nhân tài,				|B. Công bằng xã hội trong giáo dục.
+|C. Hợp tác quốc tế về giáo dục.			|D. Thực hiện công bằng giáo dục
+|?|A|-|
+ Để đổi mới cơ chế quản lí khoa học và công nghệ. Nhà nước huy động các nguồn lực đi nhanh vào một số lĩnh vực sử dụng
+|?|A. công nghệ tiên tiến.				|B. lao động thủ công.
+|C. phương pháp truyền thống.			|D. công cụ thô sở.
+|?|A|-|
+Một trong những phương hướng cơ bản để phát triển khoa học và công nghệ ở nước ta được thể hiện ở nội dung nào sau đây?
+|?|A. Đổi mới cơ chế quản lí.				|B. Khai thác mọi nguồn tài nguyên.
+|C. San bằng trình độ dân trí.				|D. Phát triển lao động thủ công.
+|?|A|-|
+Đối với nước ta hiện nay, để có thị trường khoa học và công nghệ đòi hỏi nhà nước phải thúc đẩy nội dung nào dưới đây?
+|?|A. xây dựng thiết chế văn hóa.			|B. làm du lịch cộng đồng.
+|C. đảm bảo an sinh xã hội.				|D. áp dụng khoa học, công nghệ.
+|?|D|-|
+Một trong những phương hướng xây dựng nền văn hóa tiên tiến, đậm đà bản sắc dân tộc là ke thira và phát huy những
+|?|A. phong tục lạc hậu.					|B. lối sống thực dụng.
+|C. tư tưởng mê tín dị đoàn.				|D. di sản văn hóa dân tộc.
+|?|D|-|
+Đối với đất nước ta hiện nay. Đảng ta xác định một trong những nội dung của xây dựng nền văn hóa tiên tiến đó là phải thể hiện được
+|?|A. phong tục lạc hậu.					|B. lối sống thực dụng.
+|C. tư tưởng mê tín dị đoạn				|D. tinh thần yêu nước
+|?|D|-|
+Đối với đất nước ta hiện nay, Đảng ta xác định một trong những nội dung của xây dựng nền văn hóa tiên tiến đó là phải thể hiện được
+|?|A. Lý tưởng độc lập dân tộc				|B. Chủ nghĩa cá nhân,
+|C. Dĩ công vị tư					|D. duy trì hủ tục vùng, miền
+|?|A|-|
+Một trong các phương hướng cơ bản nhằm tăng cường quốc phòng và an ninh đó là phải phát huy sức mạnh tổng hợp của khối
+|?|A. công an nhân dân.					|B. đại đoàn kết toàn dân tộc.
+|C. quân đội nhân dân.				|D. giai cấp công nhân.
+|?|B|-|
+Một trong những nguyên tắc của chính sách đối ngoại ở nước ta hiện nay là
+|?|A. Tôn trong lẫn nhau.				|B. Đề cao vị trí độc tôn.
+|C. Vận động hành lang				|D. Xuất khẩu lao động.
+|?|A|-|
+Việc làm nào dưới đây thể hiện trách nhiệm của công dân trong việc tham gia xây dựng nhà nước pháp quyền xã hội chủ nghĩa Việt Nam?
+|?|A. Hút thuốc lá nơi công cộng.			|B. Khai thuộc tài nguyên trái phép
+|C. Từ chối cách li y tế tập trung			|D. Đấu tranh tố giác tội phạm
+|?|D|-|
+Theo quy định của pháp luật, nội dung cơ bản của dân chủ trong lĩnh vực chính trị thể hiện ở việc mọi công dân đều có quyền được
+|?|A. thực hiện chính sách tương trợ			|B. thực hiện việc san bằng lợi nhuận.
+|C. thống nhất địa điểm sư trú.			|D. ứng cử hội đồng nhân dân xã
+|?|D|-|
+Việc làm nào dưới đây thể hiện trách nhiệm của công dân trong việc thực hiện tốt chính sách dân số của nhà nước
+|?|A. Chủ động mở rộng thị trưởng			|B. Chấp hành chính sách dân số,
+|C. Lựa chọn nghề nghiệp phù hợp			|D. Nội dung thông cáo báo chí
+|?|B|-|
+Việc làm nào sau đây thể hiện trách nhiệm của công dân trong việc thực hiện chính sách tài nguyên và bảo về môi trường ?
+|?|A. Xã thải trực tiếp ra môi trường.			|B. Phân loại các thải tại nguồn.
+|C. Buôn bán hàng kém chất lượng.			|D. Làm giả nhãn hiệu hàng hóa.
+|?|B|-|
+Việc làm nào sau đây không thể hiện trách nhiệm của công dân đối với chính sách giáo dục và đào tạo?
+|?|A. Phê phán mọi hình thức học tập.			|B. Nâng cao trình độ học vấn.
+|C. Đối mới phương pháp học tập.			|D. Mở rộng quy mô các cấp học.
+|?|A|-|
+Việc làm nào sau đây thể hiện trách nhiệm của công dân đối với chính sách giáo dục và đào tạo?
+|?|A. Tích cực học tập nâng cao trình độ.		|B. Xuyên tạc quan điểm giáo dục.
+|C. Gian lần trong thi và kiểm tra.			|D. Gian lặn chế độ cử tuyển.
+|?|A|-|
+Việc làm nào dưới đây là góp phần bảo tồn di tích lịch sử?
+|?|A. Từ bỏ, phục hồi giá trị di tích lịch sử		|B. Tháo đỡ, phá hủy di tích lịch sử
+|C. Cải tạo, thay thế di tích lịch sử 			|D. Giữ nguyên hiện trạng di tích lịch sử
+|?|D|-|
+Việc làm nào sau đây thể hiện nhà nước đổi mới cơ chế quản lí khoa học và công nghệ?
+|?|A. Nâng cao chất lượng đội ngũ nghiên cứu khoa học.
+|B. Nâng cao số lượng đội ngũ nghiên cứu khoa học.
+|C. Dầu tư ngân sách vào các chương trình nghiên cứu quốc gia.
+|D. Tạo ra thị trường cạnh tranh bình đẳng.
+|?|C|-|
+Hành vì nào dưới đây góp phần giữ gin, bảo vệ di sản văn hóa
+|?|A. Đảo bởi trái phép địa điểm khảo cổ		|B. Sưu tầm di vật, cổ vật
+|C. Mua bán, trao đổi trái phép bảo vật quốc gia	|D. Phá bỏ những di sản văn hóa cũ
+|?|B|-|
+Hành vi nào dưới đây không thực hiện dùng chính sách văn hóa?
+|?|A. Bảo tồn, tu bổ các di tích lịch sử			|B. Tổ chức lễ hội Hùng Vương hàng năm
+|C. Phá bỏ đình chùa, đền miếu 			|D. Tổ chức các lễ hội truyền thống
+|?|C|-|
+Công dân không thể hiện trách nhiệm đối với chính sách quốc phòng và an ninh khi thực hiện hành vi nào sau đây?
+|?|A. Gây nổi trật tự công cộng,				|B. Tham gia hoạt động quốc phòng nơi cư trú,
+|C. Gìn giữ an ninh quốc gia				|D. Thường xuyên nâng cao tinh thần cảnh giác.
+|?|A|-|
+Việc làm nào dưới đây thể hiện trách nhiệm của công dân trong việc thực hiện chính sách đối ngoại?
+|?|A. Tích cực học tập ngoại ngữ			|B. Xâm phạm lãnh thổ nước khác.
+|C. Xuyên tạc về nước khác.				|D. Bài trừ văn hóa nước ngoài.
+|?|A|-|
+Trên cơ sở quy định pháp luật về trật tự an toàn đô thị, đội trật tự của phường X - Thành phố VT dù yêu cầu mọi người không được bán hàng trên vỉa hè để đảm bảo văn minh đô thị. Việc làm này là biểu hiện của việc thực hiện chức năng nào dưới đây của nhà nước ta?
+|?|A. Đảm bảo trật tự an toàn xã hội			|B. Tổ chức thực hiện quyền công dân
+|C. Xây dựng và phát triển kinh tế.			|D. Đảm bảo quyền của công dân.
+|?|A|-|
+Anh A bị một tổ chức chuyên kích động, phá hoại an ninh đất nước mua chuộc và lôi kéo tham gia vào tổ chức đó. Là một công dân anh A chọn cách ứng xử nào dưới dây cho phù hợp với trách nhiệm của công dân khi tham gia xây dựng nhà nước pháp quyền xã hội chủ nghĩa?
+|?|A. Lờ đi coi như không biết				|B. Vui vẻ tham gia vào tổ chức đó
+|C. Rủ thêm một số người tham gia			|D. Báo cho cơ quan nhà nước có thẩm quyền biết
+|?|D|-|
+Công dân 1 tham gia thảo luận cho dự án định cạnh định cư. giải phóng mặt bằng, là định cư của huyện Y và đưa ra những góp ý xác đảng cho dự án. Điều này cho thấy công dân T đủ thực hiện quyền dân chủ trong lĩnh vực nào dưới dây?
+|?|A. Xã hội.			|B. Chính trị			|C. Văn hóa			|D. Kinh tế.
+|?|A|-|
+Trong ngày bầu cử đại biểu Hội đồng nhân dân các cấp, vì bà K phải điều trị chấn thương cột sống tại bệnh viện nên nhân viên tổ bầu cử là anh A đã tự động viết phiếu bầu thay bà K theo ý của anh và bỏ phiếu bầu đó vào hòm phiếu. Bà K bị vi phạm quyền dân chủ trên lĩnh vực nào của công dân?
+|?|A. Văn hóa			|B. Chính trị			|C. Xã hội.			|D. Kinh të.
+|?|B|-|
+Vợ chồng M sinh được hai cô con gái nên anh K chồng của M muốn sinh thêm con trai để nổi dõi tông đường. Nhưng chị lại không muốn vì cho rằng dù con gái hay con trai thì chỉ hai là đủ. Bà C là mẹ đẻ anh K đã nhờ chị X là cán bộ dân số đến giải thích cho anh K hiểu để từ bỏ ý định sinh thêm con. Ai là người thực hiện tốt chính sách dân số?
+|?|A. Anh K.			|B. M, C và X.			|C. C và X			|D. Bà C và chị M.
+|?|B|-|
+Sau khi đi picnic tại rừng, nhóm bạn gồm H, K, N, V trước khi ra về không dập tắt lửa đã đốt dẫn đến cháy rừng, gây thiệt hại 2ha. Cũng có mặt tại buổi picnic, nhưng do mệt nên T và R đã về trước đó. Trong trường hợp này, những ai đã vi phạm chính sách về bảo vệ môi trường?
+|?|A. Bạn K, H và N					|B. Bạn H, K, N, V và T
+|C. Bạn H, K, N, V và R				|D. Bạn H, K, N và V.
+|?|D|-|
+Các bạn học sinh trường THPT X tham gia tích cực cuộc thi sáng tạo khoa học kỹ thuật cấp tỉnh. Việc làm đó thể hiệu trách nhiệm nào của công dân đối với chính sách khoa học và công nghệ?
+|?|A. Tham gia nhiệt tình hoạt động phong trào. 	|B. Chiếm lĩnh kiến thức khoa học kỹ thuật.
+|C. Nâng cao trình độ học vấn.			|D. Tiếp thu tỉnh hoa văn hóa nhân loại.
+|?|B|-|
+Học sinh trường PTDTNT tỉnh X mặc trang phục truyền thống của dân tộc mình vào thứ 2 hàng tuần là thể hiện trách nhiệm của công dân đối với chính sách nào sau đây?
+|?|A. Khoa học và công nghệ.				|B. Dân tộc.
+|C. Giáo dục và đào tạo.				|D. Văn hóa.
+|?|D|-|
+Tại cuộc họp thôn X để bình xét các lao động đủ điều kiện được nhà nước hỗ trợ đi học nghề. Do đã nhận tiền của anh D từ trước nên ông B trưởng thôn đã đưa D và H vào danh sách được tham gia đợt này. Thấy D và H dù không đủ điều kiện vẫn được tham gia các lớp học nghề miễn phí. Anh T đã làm đơn khiếu nại lên UBND huyện. Sau khi xem xét, ông Q cán bộ phòng lao động thương binh xã hội đã chấp nhận để T được tham gia lớp đào tạo với điều kiện T rút đơn khiếu nại về nhưng anh không đồng ý. Những ai dưới đây đã thực hiện chưa đúng chức năng quản lý nhà nước trong quá trình thực hiện chính sách giải quyết việc làm của nhà nước ta?
+|?|A. Anh D, anh H, ông B và ông Q.			|B. Anh D, anh H.
+|C. Ông B và ông Q.					|D. Anh D, anh H, ông B và anh T.
+|?|A|-|
+Anh D và ông H là chủ của hai xưởng sản xuất đồ thủ công mĩ nghệ. Thấy cơ sở của ông H làm ăn phát đạt, anh D thuê anh M tìm hiểu và phát hiện cơ sở của ông H thường xuyên xả chất thải chưa qua xử lý ra môi trường. Biết nước thải của cơ sở mình cũng chưa qua xử lý lại lo sợ bị phát hiện. Anh D đã làm đơn tố cáo lên cơ quan chức năng. Sau khi kiểm tra anh L cán bộ phòng tài nguyên môi trường huyện đã xử phạt hành chính và đình chỉ hoạt động của cơ sở ông H để khắc phục làm ông H rất bực tức. Nhân buổi họp thôn bàn về việc tổ chức lễ hội làng. Ông H đã đề nghị anh Y trưởng thôn, gạt vợ và con gái anh D ra khỏi danh sách tham già đội văn nghệ vì gái đình anh H sinh ba con gái. Những ai dưới đây vi phạm chính sách tài nguyên bảo vệ môi trường
+|?|A. Anh D và anh L		|B. Ông H và anh Y		|C. Ông H và anh L		|D. Anh D và ông H
+|?|D|-|
+Do gia đình anh D và chị H chưa nộp một số khoản đóng góp cho làng nên ông L trưởng thôn đã loại cháu T là con gái anh D ra khỏi danh sách tham gia biểu diễn văn nghệ trong lễ hội làng làm cháu rất buồn. Trong một buổi đã nhận, anh D đem chuyện này kể với anh P một thanh niên trong làng và nói sẽ tìm cách trả thù ông L, được anh P cung cấp cho anh D một số hình ảnh về ông L cùng một số người dân trong làng thường xuyên tổ chức một số hoạt động mê tin dị đoan và truyền bá đạo trái phép. Anh D làm đơn tố cáo lên cơ quan chức năng. Trong trường hợp này ai vi phạm quyền dân chủ trên lĩnh vực văn hóa, vừa vi phạm chính sách văn hóa của nhà nước
+|?|A. Ông L.			|B. Anh D			|C. Chị H.			|D. Anh P.
+|?|A|-|
+Được chị H đề nghị, anh D đồng ý cho anh chị H sử dụng bản thiết kế dây chuyền xử là nước thải. Sau khi sử dụng, chị H cùng với anh T đã nghiên cứu cải tiến thêm một số nội dung để dây chuyền được hoàn thiện. Sau đó anh T làm hồ sơ đăng ký bản quyền tại cơ quan chức năng dưới danh nghĩa chủ sở hữu là chị H và anh T. Phát hiện thấy dây chuyển mới đăng ký của chị H và anh T đã sao chép một phần lớn đề tài của mình mà không xin ý kiến, anh D làm đơn khiếu nại. Sau khi xem xét, cơ quan chức năng kết luận, hồ sơ đăng ký sở hộ trí tuệ của anh T và chị H là hợp lệ, còn khiếu nại của anh D là chưa có cơ sở vì anh D chưa đăng ký bảo hộ sở hữu trí tuệ. Trong trường hợp này ai đã thực hiện chưa đúng quyền sở hữu công nghiệp?
+|?|A. Anh D			|B. Anh D và chị H		|C. Chị H			|D. Anh D và anh T.|?|B`;
+    let body = $("body");
+    let questionsList = questions.split('|-|');
+    let data = [];
+    questionsList.forEach(item => {
+        let question = item.split('|?|')[0].trim();
+        let answers = (item.split("|?|")[1]);
+        let rightAnswer = item.split("|?|")[2];
+
+        answers = ('' + answers).split("|");
+        answers = answers.map(function(i) {
+            return i.trim();
+        });
+        answers = answers.filter(function(i) {
+            return i != '';
+        });
+        let newItem = [];
+        newItem['question'] = question;
+        newItem['answers'] = answers;
+        newItem['rightAnswer'] = rightAnswer;
+        data.push(newItem);
+    });
+    console.log(data);
+    data.forEach((item, index) => {
+      let qAnswer = `<button>${item.answers[0]}</button>
+        <button>${item.answers[1]}</button>
+        <button>${item.answers[2]}</button>
+        <button>${item.answers[3]}</button>`;
+      let e = `<div class="question" data-answer="${item.rightAnswer}">
+        <h3>Câu ${index + 1}. ${item.question}</h3>
+        <div>
+        ${qAnswer}
+        </div>
+        </div>`;
+      body.append(e);
+    });
+    
+})
